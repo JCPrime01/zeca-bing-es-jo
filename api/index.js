898d5ed18f8062c3ab1111b6367488a5ba1dc63d
JCPrime01 <vanzokiller@gmail.com>
2026-06-01 11:03:39 -0300

    chore: shareCode 7K5LEGR8XUQ -> U0FB2BE3H2F (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=7K5LEGR8XUQ&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=U0FB2BE3H2F&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
