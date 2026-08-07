b3bb896f0bca5d6096d720ce2d45fec52a8928da
JCPrime01 <vanzokiller@gmail.com>
2026-08-07 14:02:37 -0300

    chore: shareCode B8OOTQKUDAR -> 2C6IZVTPBRI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=B8OOTQKUDAR&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=2C6IZVTPBRI&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
