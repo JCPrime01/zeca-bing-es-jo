18259cb5aeccc5313d903e621467e8cd89978217
JCPrime01 <vanzokiller@gmail.com>
2026-08-08 12:13:51 -0300

    chore: shareCode 2C6IZVTPBRI -> 1QVRX8QVZA4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=2C6IZVTPBRI&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=1QVRX8QVZA4&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
