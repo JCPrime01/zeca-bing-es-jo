4de39e784946fb4bb26aa79a95933c7f6cafd9d7
JCPrime01 <vanzokiller@gmail.com>
2026-08-11 10:22:43 -0300

    chore: shareCode Y55ET1CEGOB -> 0C302SC1GOI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=Y55ET1CEGOB&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=0C302SC1GOI&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
